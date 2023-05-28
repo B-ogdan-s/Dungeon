9dfa9afbae0db5d91de1cdf5f54045492b32ab1b
Bogdan_s <bogdans20o62002@gmail.com>
2023-05-28 13:12:41 +0300

    ,

--- Assets/Scripts/Weapon/Bullet/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet/Bullet.cs
@@ -7,10 +7,17 @@ public class Bullet : MonoBehaviour
     [SerializeField, Min(0)] private int _damage;
     [SerializeField, Min(0)] private float _timeLive;
 
+    private Rigidbody _rigidbody;
     private Coroutine _liveCoroutine;
 
-    private void Update()
+    private void Start()
     {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        //_rigidbody.MovePosition(transform.forward * _speed * Time.fixedDeltaTime + transform.position);
         transform.Translate(Vector3.forward * _speed * Time.fixedDeltaTime);
     }
 
